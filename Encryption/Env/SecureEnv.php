a85ff1daf6551c4cac64039b75d4fe4300434c7a
Joshua Parker <joshua@joshuaparker.dev>
2025-09-27 23:15:20 -0700

    Updated parameters.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- Encryption/Env/SecureEnv.php
+++ Encryption/Env/SecureEnv.php
@@ -4,24 +4,30 @@ declare(strict_types=1);
 
 namespace Qubus\Http\Encryption\Env;
 
+use Defuse\Crypto\Exception\BadFormatException;
 use Defuse\Crypto\Exception\EnvironmentIsBrokenException;
 use Defuse\Crypto\Exception\WrongKeyOrModifiedCiphertextException;
 use Defuse\Crypto\Key;
+
+use function file_get_contents;
 
 class SecureEnv
 {
     /**
      * @throws EnvironmentIsBrokenException
      * @throws WrongKeyOrModifiedCiphertextException
+     * @throws BadFormatException
      */
-    public function parse(string $input, Key $key): void
+    public static function parse(string $inputFile, string $keyFile): void
     {
-        if ($decrypted = File::decrypt($input, $key)) {
+        $keyString = file_get_contents($keyFile);
+
+        if ($decrypted = File::decrypt($inputFile, Key::loadFromAsciiSafeString($keyString))) {
             $parsed = Parser::parse($decrypted) ?? [];
 
             foreach ($parsed as $key => $value) {
                 if (isset($parsed[$key]) && !empty($parsed[$key])) {
-                    putenv($key . '=' . $value);
+                    putenv(assignment: $key . '=' . $value);
                 }
             }
         }
